06ec11f2f7c06f9b4f2c46ce98d5b0619cdf8860
sandeshnimat <sandeshnimat2895@gmail.com>
2020-07-07 15:53:10 -0400

    Updates

---
+++ precache-manifest.6f79d222520a5264d14cc777706d5ad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2293aa99e8784f210647380a82f1b8b4",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "26dd4c8c4cb46f1cba13",
+    "url": "/me/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e86032bfd5d5ce9574ac",
+    "url": "/me/static/js/2.7fdd50ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/me/static/js/2.7fdd50ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26dd4c8c4cb46f1cba13",
+    "url": "/me/static/js/main.14effb7d.chunk.js"
+  },
+  {
+    "revision": "be4ab76b74ce08113e38",
+    "url": "/me/static/js/runtime-main.a62d5722.js"
+  }
+]);
